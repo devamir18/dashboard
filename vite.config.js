379809d310e291b786d0b290ee852f6b782cf8a6
devamir18 <abualiusman14741@gmail.com>
2025-12-10 19:35:47 +0100

    update
    asap

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/Hr-mini System/",
+    base: "/dashboard/",
   plugins: [react(),tailwindcss()],
 })
